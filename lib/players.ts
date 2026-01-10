b9b01a70330a008361b980aeaa396de9e9532dcd
kelvincedcas <kelvincedcas@gmail.com>
2026-01-09 21:48:06 -0500

    add more players tag

--- lib/players.ts
+++ lib/players.ts
@@ -4,19 +4,22 @@
 
 export const TOURNAMENT_PLAYERS = [
   { nickname: 'lorito90', tag: 'LAN', role: 'SUPPORT' },
-  { nickname: 'Kasilio', tag: 'LAN', role: 'SUPPORT' },
+  { nickname: 'Kasilio', tag: '1115', role: 'SUPPORT' },
   { nickname: 'DarKofHelL', tag: 'LAN', role: 'MID' },
-  { nickname: 'Report a mi Zed', tag: 'LAN', role: 'MID' },
-  { nickname: 'the gout', tag: 'LAN', role: 'TOP' },
+  { nickname: 'Report a mi Zed', tag: '9620', role: 'MID' },
+  { nickname: 'the gout', tag: '9912', role: 'TOP' },
   { nickname: 'Chamemohi', tag: 'LAN', role: 'ADC' },
-  { nickname: '火 paLoma', tag: 'LAN', role: 'ADC' },
+  { nickname: '火 paLoma', tag: '1996', role: 'ADC' },
   { nickname: 'Bochamohi', tag: 'LAN', role: 'SUPPORT' },
+  { nickname: 'Erosannin', tag: '1986', role: 'JUNGLE' },
   { nickname: 'flawer1212', tag: 'LAN', role: 'MID' },
   { nickname: 'Paulsemohi', tag: 'LAN', role: 'JUNGLE' },
   { nickname: 'iBuprofeno500mg', tag: 'LAN', role: 'SUPPORT' },
   { nickname: 'Guada', tag: 'LAN', role: 'MID' },
   { nickname: 'No lo zed', tag: 'LAN', role: 'MID' },
-  { nickname: 'NickGL12', tag: 'LAN', role: 'MID' }, // asumido
+  { nickname: 'NickGL12', tag: '3919', role: 'MID' }, // asumido
   { nickname: 'Ivermectina6m', tag: 'LAN', role: 'SUPPORT' },
   { nickname: 'Rudamohi', tag: 'LAN', role: 'JUNGLE' },
+  { nickname: 'Sanji Ec', tag: 'LAN', role: 'JUNGLE' },
+  { nickname: 'Boa HancocK', tag: 'GPI', role: 'JUNGLE' },
 ];
